2b0ca3581a5096b978733889c261ad0a72375018
il12 <il5498@yandex.ru>
2016-12-09 04:01:45 +0300

    Добавлены логотипы и обновлены файлы прототипа

--- УПП/scripts.js
+++ УПП/scripts.js
@@ -1,49 +1,49 @@
 data = [{
-		"pic": "Picture",
+		"pic": "./pictures/SemRush.png",
 		"company": "SemRush",
-		"company_href": "Ссылка на компанию",
+		"company_href": "Ссылка на компанию 1",
 		"vacancy": "JS-разработчик",
-		"vacancy_href": "Ссылка на вакансию",
+		"href": "Ссылка на вакансию 1",
 		"salary": 50000,
 		"lang": "JavaScript",
 		"exp": 5,
 		"city": "Москва"
 	}, {
-		"pic": "Picture",
+		"pic": "./pictures/Google.png",
 		"company": "Google",
-		"company_href": "Ссылка на компанию",
+		"company_href": "Ссылка на компанию 2",
 		"vacancy": "Генератор идей",
-		"href": "Ссылка на вакансию",
+		"href": "Ссылка на вакансию 2",
 		"salary": 150000,
 		"lang": "Python",
 		"exp": 3,
 		"city": "Санкт-Петербург"
 	}, {
-		"pic": "Picture",
+		"pic": "./pictures/Yandex.png",
 		"company": "Yandex",
-		"company_href": "Ссылка на компанию",
+		"company_href": "Ссылка на компанию 3",
 		"vacancy": "Рыбатель со стажем",
-		"href": "Ссылка на вакансию",
+		"href": "Ссылка на вакансию 3",
 		"salary": 1500000,
 		"lang": "Lua",
 		"exp": 10,
 		"city": "Санкт-Петербург"
 	}, {
-		"pic": "Picture",
+		"pic": "./pictures/FakeCompany.png",
 		"company": "FakeCompany",
-		"company_href": "Ссылка на компанию",
+		"company_href": "Ссылка на компанию 4",
 		"vacancy": "Директор",
-		"href": "Ссылка на вакансию",
+		"href": "Ссылка на вакансию 4",
 		"salary": 60000,
 		"lang": "C#",
 		"exp": 3,
 		"city": "Хельсинки"
 	}, {
-		"pic": "Picture",
+		"pic": "./pictures/FakeCompany_2.png",
 		"company": "FakeCompany 2",
-		"company_href": "Ссылка на компанию",
+		"company_href": "Ссылка на компанию 5",
 		"vacancy": "Уборщик",
-		"href": "Ссылка на вакансию",
+		"href": "Ссылка на вакансию 5",
 		"salary": 30000,
 		"lang": "С++",
 		"exp": 20,
@@ -61,23 +61,24 @@ function start() {
 
 	for (i = 0; i < data.length; i++) {
 		let div = document.createElement("div");
+				
 		div.classList.add("panel-heading");
 		div.classList.add("main_content_class");
 		div.classList.add("col-md-12");
 		div.innerHTML = form_result(data[i]);
+
+		div.addEventListener("click",transfer_to.bind(null,data[i].href));
 		con.appendChild(div);
 	}
 
 }
 
-function form_result(data) {
-	return '<div class="test col-md-8"><dl><dt><a href=' + data.href + '>' + data.vacancy + '</a></dt><dd>Компания: <span class="company_span">' + data.company + '</span></dd></dl> </div> <div class="col-md-4"><h3 class="text-success">' + data.salary + ' \u20BD</h3></div>'
+function transfer_to(link) {
+ window.location = link;
 }
 
-// let con = document.getElementById('main_content');
-// for(i=0;i<data.length;i++){
-//	let div = document.createElement("div");
-//	div.classList.add("main_content_class");
-//   div.innerHTML = '<dl><dt>' + data[i].vacancy + '</dt><dd>Компания: ' + data[i].name + '</dd></dl>'
-//	con.appendChild(div);
-// }
+function form_result(data) {
+	return '<div class="test col-md-8"><div class="col-md-3"><img src="' + data.pic + '"></div><dl><dt>' + data.vacancy + '</a></dt><dd>Компания: <span class="company_span">' + data.company + '</span></dd></dl> </div> <div class="col-md-4"><h3 class="text-success">' + data.salary + ' \u20BD</h3></div>'
+}
+
+
